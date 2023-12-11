530aaef57eeff57bbdc91ee394f0da5f368101a0
Jonasthepanda67 <jonas.petersen@danfoss.com>
2023-12-11 13:25:14 +0100

    update

--- Regnvejrsstatistik/Program.cs
+++ Regnvejrsstatistik/Program.cs
@@ -1,1 +1,35 @@
-﻿Console.WriteLine("Hello, World!");
+﻿namespace RVS
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            double[] nedbøruge = new double[7];
+
+            Console.Write("Indtast nedbør for mandag her: ");
+            nedbøruge[0] = double.Parse(Console.ReadLine());
+            Console.Write("Indtast nedbør for tirsdag her: ");
+            nedbøruge[1] = double.Parse(Console.ReadLine());
+            Console.Write("Indtast nedbør for onsdag her: ");
+            nedbøruge[2] = double.Parse(Console.ReadLine());
+            Console.Write("Indtast nedbør for torsdag her: ");
+            nedbøruge[3] = double.Parse(Console.ReadLine());
+            Console.Write("Indtast nedbør for fredag her: ");
+            nedbøruge[4] = double.Parse(Console.ReadLine());
+            Console.Write("Indtast nedbør for lørdag her: ");
+            nedbøruge[5] = double.Parse(Console.ReadLine());
+            Console.Write("Indtast nedbør for søndag her: ");
+            nedbøruge[6] = double.Parse(Console.ReadLine());
+            Console.Clear();
+            for (var i = 0; i < 7; i++)
+            {
+                int l = i + 1;
+                Console.WriteLine("-     Nedbør værdi {0} = {1} mm", l, nedbøruge[i]);
+            }
+            double ave = nedbøruge.Average();
+            Console.WriteLine("\nGennemsnittet er: {0}", Math.Round(ave, 2));
+            Console.WriteLine("Den laveste værdi er: {0}", nedbøruge.Min());
+            Console.WriteLine("Den største værdi er: {0}", nedbøruge.Max());
+        }
+    }
+}
---
+++ Regnvejrsstatistik/README.md
@@ -0,0 +1,32 @@
+# README.md
+
+Den første del af programmet beder brugeren om 7 inputs af nedbør som tæller for en hel uge.
+
+De 7 inputs bliver lagt i en double array så der også kan være komma værdier.
+
+Når det hele er indtastet så vil arrayen gå igennem et loop som udskriver værdierne i arrayet i samme rækkefølge som de blev intastet.
+Og den skriver det i et format af en liste som skriver værdierne i mm.
+
+Ved hjælp af variablen ave finder jeg gennemsnittet af de 7 tal og runder tallet op så det står i 2 decimaler.
+
+Ved hjælp af .Min funktionen finder jeg den laveste værdi ud af de 7 værdier og udskriver den.
+
+Ved hjælp af .Max funktionen finder jeg den højeste værdi ud af de 7 værdier og udskriver den.
+
+Et eksempel på dette program kunne være at skrive disse værdier: 7 3,8 5 2 8,4 9 2,5
+
+Man ville til sidst få disse værdier ud af konsollen.
+
+-     Nedbør værdi 1 = 7 mm
+-     Nedbør værdi 2 = 3,8 mm
+-     Nedbør værdi 3 = 5 mm
+-     Nedbør værdi 4 = 2 mm
+-     Nedbør værdi 5 = 8,4 mm
+-     Nedbør værdi 6 = 9 mm
+-     Nedbør værdi 7 = 2,5 mm
+
+Gennemsnittet er: 5,39
+
+Den laveste værdi er: 2
+
+Den største værdi er: 9
